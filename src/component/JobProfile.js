5579e71db54bffa038150188636e58d497424dc8
Kaushik Komanduri <komanduri.k@northeastern.edu>
2022-12-11 15:15:59 -0500

    profile and job description details

--- src/component/JobProfile.js
+++ src/component/JobProfile.js
@@ -4,10 +4,14 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import apiList from "../lib/apiList";
 import { SetPopupContext } from "../App";
+import moment from "moment";
+import { Button, Card, Grid } from '@material-ui/core';
 
 const JobProfile = () => {
   const { jobId } = useParams();
   const [jobDetails, setJobDetails] = useState("");
+  const [dateofOperation, setdateofOperation] = useState("2015-03-04T00:00:00.000Z");
+  const [deadLine, setDeadline] = useState("2015-03-04T00:00:00.000Z");
   const setPopup = useContext(SetPopupContext);
 
   useEffect(() => {
@@ -23,7 +27,10 @@ const JobProfile = () => {
       })
       .then((response) => {
         console.log(response.data);
+        setdateofOperation(moment(response.dateOfPosting).utc().format('YYYY-MM-DD'));
+        setDeadline(moment(response.dateOfPosting).utc().format('YYYY-MM-DD'));
         setJobDetails(response.data);
+        
       })
       .catch((err) => {
         console.log(err.response.data);
@@ -35,7 +42,110 @@ const JobProfile = () => {
       });
   };
 
-  return <div>Make job details ui</div>;
+  return(
+    <div>
+      <h1>Job Description</h1>
+<Card>
+<Grid container spacing={2}>
+  <Grid item xs={4}>
+   <b> Name:</b> 
+  </Grid>
+  <Grid item xs={8}>
+  {jobDetails.title}
+  </Grid>
+  <Grid item xs={4}>
+  <b> Accepted Appplications:</b> 
+  </Grid>
+  <Grid item xs={8}>
+  {jobDetails.activeApplications}
+  </Grid>
+  <Grid item xs={4}>
+  <b>Accepted Candidates: </b>
+  </Grid>
+  <Grid item xs={8}>
+  {jobDetails.acceptedCandidates}
+  </Grid>
+  <Grid item xs={4}>
+  <b>Blocked:</b>
+  </Grid>
+  <Grid item xs={8}>
+  {jobDetails.blocked == false ? 'Yes' : 'No'}
+  </Grid>
+  <Grid item xs={4}>
+  <b>Date of Posting:</b>
+  </Grid>
+  <Grid item xs={8}>
+  {dateofOperation}
+  </Grid>
+  <Grid item xs={4}>
+  <b>Deadline:</b>
+  </Grid>
+  <Grid item xs={8}>
+  {deadLine}
+  </Grid>
+  <Grid item xs={4}>
+  <b>Duration:</b>
+  </Grid>
+  <Grid item xs={8}>
+  {jobDetails.duration}
+  </Grid>
+  <Grid item xs={4}>
+  <b>External Job Id:</b>
+  </Grid>
+  <Grid item xs={8}>
+  {jobDetails.externalJobId}
+  </Grid>
+  <Grid item xs={4}>
+  <b>Job Type:</b>
+  </Grid>
+  <Grid item xs={8}>
+  {jobDetails.jobType}
+  </Grid>
+  <Grid item xs={4}>
+  <b>Maximum Applications:</b>
+  </Grid>
+  <Grid item xs={8}>
+  {jobDetails.maxApplicants}
+  </Grid>
+  <Grid item xs={4}>
+  <b>Maximum Positions: </b>
+  </Grid>
+  <Grid item xs={8}>
+  {jobDetails.maxPositions}
+  </Grid>
+  <Grid item xs={4}>
+  <b>Skills:</b>
+  </Grid>
+  <Grid item xs={8}>
+  {jobDetails.maxApplicants}
+  </Grid>
+  <Grid item xs={4}>
+  <b>Rating:</b>
+  </Grid>
+  <Grid item xs={8}>
+  {jobDetails.rating == -1 ? 0 : jobDetails.rating}
+  </Grid>
+  <Grid item xs={4}>
+  <b>Salary: </b>
+  </Grid>
+  <Grid item xs={8}>
+  ${jobDetails.salary} per annum
+  </Grid><br/>
+  <Grid item xs={1}>
+  <Button variant="contained" color="secondary">
+  Back
+</Button>
+  </Grid>
+  <Grid item xs={2}>
+<Button variant="contained" color="primary">
+  Apply
+</Button>
+  </Grid>
+</Grid>
+
+</Card>
+</div>
+  ); 
 };
 
 export default JobProfile;
--- src/component/PublicProfile.js
+++ src/component/PublicProfile.js
@@ -4,10 +4,12 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import apiList from "../lib/apiList";
 import { SetPopupContext } from "../App";
+import { Button, Card, Grid } from '@material-ui/core';
 
 const PublicProfile = () => {
   const { profileId } = useParams();
   const [profileDetails, setProfileDetails] = useState("");
+  const [eduDetails, setEduDetails] = useState([]);
   const setPopup = useContext(SetPopupContext);
 
   useEffect(() => {
@@ -24,6 +26,7 @@ const PublicProfile = () => {
       .then((response) => {
         console.log(response.data);
         setProfileDetails(response.data);
+        setEduDetails(response.data.education)
       })
       .catch((err) => {
         console.log(err.response.data);
@@ -35,7 +38,81 @@ const PublicProfile = () => {
       });
   };
 
-  return <div>Make profile screen ui</div>;
+  return (
+    <div>
+<h1>Candidate Profile</h1>
+<Card>
+<Grid container spacing={2}>
+  <Grid item xs={4}>
+   <b> Name:</b> 
+  </Grid>
+  <Grid item xs={8}>
+  {profileDetails.name}
+  </Grid>
+  <Grid item xs={4}>
+  <b> Education:</b> 
+  </Grid>
+  <Grid item xs={8}>
+  {eduDetails.map(y=>{
+    return(
+      <div>
+        <Grid container spacing={2}>
+         <Grid item xs={2}>
+          <i> University:</i> 
+         </Grid>
+         <Grid item xs={3}>
+          {y.institutionName}
+         </Grid>
+        <Grid item xs={2}>
+          <i> Start Year:</i> 
+        </Grid>
+        <Grid item xs={1}>
+        {y.startYear}
+        </Grid>
+        <Grid item xs={2}>
+        <i> End Year:</i> 
+        </Grid>
+        <Grid item xs={1}>
+        {y.endYear}
+        </Grid>
+      </Grid>
+      </div>
+  )
+  })}
+  </Grid>
+
+  <Grid item xs={4}>
+  <b> Candidate Rating:</b> 
+  </Grid>
+  <Grid item xs={8}>
+  {profileDetails.rating == -1 ? 0 : profileDetails.rating}
+  </Grid>
+  <Grid item xs={4}>
+  <b> Profile:</b> 
+  </Grid>
+  <Grid item xs={8}>
+  {profileDetails.profile}
+  </Grid>
+  <Grid item xs={4}>
+  <b> Resume:</b> 
+  </Grid>
+  <Grid item xs={8}>
+  {profileDetails.resume}
+  </Grid>
+  <Grid item xs={4}>
+  <b> Skills:</b> 
+  </Grid>
+  <Grid item xs={8}>
+  {profileDetails.resume}
+  </Grid>
+
+</Grid><br/>
+<Button variant="contained" color="secondary">
+  Back
+</Button>
+</Card>
+    </div>
+  )
 };
 
 export default PublicProfile;
